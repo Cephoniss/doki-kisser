0b0aeb189c6eaaf4df7d122ea280d32d3124a222
Edgardo D Acosta <eacostaiii@gmail.com>
2025-08-04 22:18:53 -0400

    Sounds and effects
    -Added logic for playing audio and visual effect when clicking on targets
    -Added additional image and sound assets
    -Adjusted scene prefabs

--- kissTarget.gd
+++ kissTarget.gd
@@ -1,8 +1,11 @@
 extends Area2D
 
+@onready var audio_player = $AudioStreamPlayer2D
+@onready var kiss_effect = $KissEffect
 @export var move_speed: float = 100.0
 @export var rotation_speed: float = 90.0
 var velocity := Vector2.ZERO
+var clicked := false
 
 func _ready():
 	add_to_group("KissScoreGroup")
@@ -17,12 +20,24 @@ func  _process(delta):
 	rotation_degrees += rotation_speed * delta
 
 func _on_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.pressed:
+	if event is InputEventMouseButton and event.pressed and not clicked:
+		clicked = true
+		audio_player.play()
+		show_kiss_effect()
+		remove_from_group("KissScoreGroup")
+		await get_tree().create_timer(0.3).timeout
 		#var spawner = get_node_or_null("/root/game_space")
 		#if spawner and spawner.has_method("spawn_random_kiss_target"):
 			#spawner.spawn_random_kiss_target()
 			#spawner.spawn_random_kiss_target()
 		queue_free()
+
+func show_kiss_effect():
+	kiss_effect.show()
+	kiss_effect.modulate.a = 1.0
+	var tween = create_tween()
+	tween.tween_property(kiss_effect, "modulate:a", 0.0, 0.3)
+	
 func despawn():
 	await get_tree().create_timer(2.0).timeout
 	queue_free()
